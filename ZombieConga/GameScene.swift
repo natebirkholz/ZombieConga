e770876a3c0dc51ef45ade360246802e689f9f71
natebirkholz <nbirkholz@gmail.com>
2017-05-30 21:47:19 -0700

    Lesson 1 done, large zombie.

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -13,5 +13,23 @@ class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+//        background.zRotation = CGFloat(Double.pi) / 8
+        background.zPosition = -1
+        addChild(background)
+
+        let zombie = SKSpriteNode(imageNamed: "zombie1")
+        zombie.position = CGPoint(x: 400, y: 400)
+        zombie.zPosition = 100
+        zombie.xScale = 2
+        zombie.yScale = 2
+        
+
+        addChild(zombie)
+
+        let mySize = background.size
+        print(mySize)
     }
 }
